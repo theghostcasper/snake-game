ddf6050c724cc0f4ccaddbaa1a0bab8103759308
theghostcasper <casper.thecuteghost@gmail.com>
2019-08-20 00:07:31 +0200

    documenting game

--- js/game.js
+++ js/game.js
@@ -1,63 +1,88 @@
-//game module
+//general game module
 let Game = () => {
-	let gameSpeed = 100; /* ms */ 
-	/* Game Play handler */
+	let gameSpeed = 120; /* ms */ 
+
+	/* Game Play handler timer handlers */
 	let gamePlay,timer;
+
+	/* stores the function to execute when game starts */
 	let playCache
 
+	/* increases the game speed */
 	function upSpeed(speed) {
 		gameSpeed -= speed;
 		clearInterval(gamePlay)
 		startGame(playCache);
 	}
 
-	function startGame(play) {
-		playCache = play
+	/* used to set the gameplay function to be stored in the playCache */
+	function passGame(gameplay) {
+		playCache = gameplay
+	}
+
+	/*  function to start playing the game */
+	function startGame() {
 		gamePlay = setInterval(function(){
-			play();
+			playCache();
 		},gameSpeed)
 	}
-	function gameTimer(handler) {
+
+	/* Function that adds extra features, it is a timer that executes every x seconds. */
+	function gameTimer(handler,seconds) {
 		timer = setInterval(function() {
 			handler();
-		},4000)
+		},seconds * 1000)
 	}
+
+	/* Function to stop the game by dispatching the handlers */
 	function stopGame() {
 		clearInterval(gamePlay);
+		clearInterval(timer);
 	}
 
 	return {
 		startGame,
 		stopGame,
 		gameTimer,
-		upSpeed
+		upSpeed,	
+		passGame
 	}
 }
 
 
 /* Snake Module */
 let Snake = (snakeElement) => {
+	
+ 	/* stores the current movement*/
 	let movement = 'up';
-	let foodObj = {}
+
+	/* stores the previous movement */
+	let previousMovement;
+
+	/* Contains the location of the food available in x, and y positions. */
+	let foodObj = {};
+
+	/* keeping a count of the score */
 	let score = 0;
+
+	/* Contains all the info about the snake and its tails */
 	let snakeObject = { 
 		name: 'snake',
 		x: 10, 
 		y: 15, 
 		headPosition: 'up',
 		tails: [
-			{name: 's0', x: 10, y: 16},
-			{name: 's1', x: 10, y: 17},
-			{name: 's2', x: 10, y: 18},
-			{name: 's3', x: 10, y: 19}
+			{name: 's0', x: 10, y: 16}
 		]
 	}  
 
+	/* apply a movement to the snake head on screen  */
 	function applyMovement() {
 		snakeElement.style.gridRow = (snakeObject.y);
 		snakeElement.style.gridColumn = (snakeObject.x);
 	}
 
+	/* add a class to the snake head to orient his head direction */
 	function orientHead() {
 		snakeElement.classList.remove('up')
 		snakeElement.classList.remove('down')
@@ -65,10 +90,14 @@ let Snake = (snakeElement) => {
 		snakeElement.classList.remove('right')
 		snakeElement.classList.add(snakeObject.headPosition);
 	}
+
+	/* used to change the movement direction of the snake either up,down, left or right */
 	function setMovementPosition(pos) {
 		movement = pos;
 	}
 
+
+	/* function to Move the head of the snakeObject */
 	function moveHead(objToMove) {
 		if(!objToMove)
 			objToMove = snakeObject;
@@ -96,18 +125,24 @@ let Snake = (snakeElement) => {
 		}
 	}
 
+	/* Remove the last tail part  and return it */
 	function popTail() {
 		const tail = snakeObject.tails.pop();
 		let tailElement = document.querySelector('.' + tail.name);
 		tailElement.parentNode.removeChild(tailElement);
 		return tail;
 	}
+
+	/* function to add a new tail part */
 	function pushTail() {
 		const parentTail = snakeObject.tails[snakeObject.tails.length-1]
 		const tail = {name: 's'+new Date().getTime(), x: parentTail.x, y: parentTail.y}
 		snakeObject.tails.push(tail)
 		snakeElement.insertAdjacentHTML("afterend", `<div class="snake-tail ${tail.name}" style="grid-column:${tail.x}; grid-row:${tail.y}"></div>`);
 	}
+	
+
+	/* Function to add a tail directly after the head */
 	function unshiftTail(tail) {
 		switch (movement) {
 			case 'up':
@@ -133,56 +168,94 @@ let Snake = (snakeElement) => {
 		}
 		snakeElement.insertAdjacentHTML("afterend", `<div class="snake-tail ${tail.name}" style="grid-column:${tail.x}; grid-row:${tail.y}"></div>`);
 	}
+ 	
+
+ 	/* Simple Algorithm for the snake tail to follow his head.
+		Simply we pop the last tail and add it after his head.
+ 	 */
  	function moveTail() {
 		let tail = popTail();
 		unshiftTail(tail);			
 	}
+	
+
+	/* Main snake move function*/
 	function move() {
+
+		/* Guard so the snake won't move over itself, by saving the previous movement */ 
+		if((movement == 'right' && previousMovement == 'left') || (movement == 'left' && previousMovement == 'right') || (movement == 'up' && previousMovement == 'down') || (movement == 'down' && previousMovement == 'up') )
+			movement = previousMovement;
+		else {
+			previousMovement = movement;
+		}
+		/* Adjust the snake head position in snake object */
 		snakeObject.headPosition = movement;
+		/* adjust the head position on screen */
 		orientHead()
+		/* adjust the snake head object positions x and y*/
 		moveHead()
-		moveTail();
+		/* Apply the movement on screen  */
 		applyMovement();
+		/* move the tail after the head*/
+		moveTail();
+
+		/* Check for any intersection after the movement */
 		checkIntersection();
 	}
+	
+
+	/* Checks any type of intersection, either snake head with food or snake head with body*/
 	function checkIntersection() {
 		let food = document.querySelector('.food')
 		let scoreElement = document.querySelector('.score-val') 
 		if(food) {
+			/* if intersection head with food, then add score and delete food. also add a tail. */
 			if(snakeObject.x == foodObj.x && snakeObject.y == foodObj.y){
 				food.parentNode.removeChild(food);
 				score++;
+				/* update the score on screen  */
+				scoreElement.innerText = score;
 				pushTail()
 			}
 		}
-		scoreElement.innerText = score;
 
-
+		/* Loop over tails to check the intersection between the snake body and his head. */
 		snakeObject.tails.forEach(tail => {
 			if(snakeObject.x == tail.x && snakeObject.y == tail.y){
-				//alert('Game Over');
-				//location.reload()		
+				alert('Game Over');
+				location.reload()		
 			}
-
 		})
 
 	}	
 
+	/* function to make the snake dash 5 blocks on the screen*/
 	function snakeDash() {
-		for(i=0;i<5;i++)
+		for(i=0;i<5;i++) {
 			move();
+		}
 	}
 
+
+	/* Function to put the snake food on a random position on the Screen */
 	function putFood(food) {
+		/* checks if there's a previous food on the screen and removes it */
 		let oldfood = document.querySelector('.food')
 		if(oldfood)
 			oldfood.parentNode.removeChild(oldfood);
+
+		/* generating random x and y positions for the food on the grid. */
 		let	xmin = 1, xmax=20, ymin=1, ymax= 20;
 		let xrand = Math.floor(Math.random() * (xmax - xmin)) + xmin
 		let yrand = Math.floor(Math.random() * (ymax - ymin)) +ymin
+
+		/* updating the position of the food object and adding the food on the screen */
 		foodObj = { x: xrand,y:yrand }
-		snakeElement.insertAdjacentHTML("afterend", `<p class="food" style="grid-column:${xrand}; grid-row:${yrand}"></p>`);
+		snakeElement.insertAdjacentHTML("afterend", `<p class="food" style="grid-column:${foodObj.x}; grid-row:${foodObj.y}"></p>`);
 	}
+
+
+
 	return {
 		move,
 		setMovementPosition,
@@ -190,6 +263,4 @@ let Snake = (snakeElement) => {
 		snakeDash,
 		score
 	}
-
-	
 }
--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
-/* Game Logic */
+(() => {
+	/* Game Logic */
+	/* Play button event listener */
+	document.querySelector('.play').addEventListener('click',function(){
+		this.style.display = 'none';
+		document.getElementById('app').style.display = 'grid';
+		document.querySelector('.main-header').style.display = 'none';
+		let startGame = setInterval(function(){
+			document.getElementById('app').style.opacity++;
+			if(document.getElementById('app').style.opacity == 1)
+				clearTimeout(startGame)
+		},10)
+	})
 
-/* Play button event listener */
-document.querySelector('.play').addEventListener('click',function(){
-	this.style.display = 'none';
-	document.getElementById('app').style.display = 'grid';
-	document.querySelector('.main-header').style.display = 'none';
-	let startGame = setInterval(function(){
-		document.getElementById('app').style.opacity++;
-		if(document.getElementById('app').style.opacity == 1)
-			clearTimeout(startGame)
-	},10)
-})
+	/* Select the snake element */
+	let snakeElement = document.getElementById('snake')
+
+	let snake = Snake(snakeElement);
+
+	/* Button event listener */
+	document.addEventListener('keydown',function(event){
+		if(event.code == 'ArrowUp') {
+			snake.setMovementPosition('up')
+		}
+		if(event.code == 'ArrowDown') {
+			snake.setMovementPosition('down')
+		}
+		if(event.code == 'ArrowRight') {
+			snake.setMovementPosition('right')
+		}
+		if(event.code == 'ArrowLeft') {
+			snake.setMovementPosition('left')
+		}
+	})
+	document.addEventListener('keyup',function(event){
+		if(event.code == 'Space')
+			snake.snakeDash()
+	})
 
 
-let snakeElement = document.getElementById('snake')
+	let scoreNow = 0; /* used as a flag */
 
-let snake = Snake(snakeElement);
 
-/* Button event listener */
-document.addEventListener('keydown',function(event){
-	if(event.code == 'ArrowUp') {
-		snake.setMovementPosition('up')
-	}
-	if(event.code == 'ArrowDown') {
-		snake.setMovementPosition('down')
-	}
-	if(event.code == 'ArrowRight') {
-		snake.setMovementPosition('right')
-	}
-	if(event.code == 'ArrowLeft') {
-		snake.setMovementPosition('left')
-	}
-})
-document.addEventListener('keyup',function(event){
-	if(event.code == 'Space')
-		snake.snakeDash()
-})
-let scoreNow = 0;
-let game = Game();
-game.startGame(function(){
-	snake.move();
-	score = parseInt(document.querySelector('.score-val').innerText);
-	if(score>0 && score % 5 == 0 && score !==scoreNow && score != 50)
-	{
-		scoreNow = score;
-		game.upSpeed(5);
-	}
-})
-game.gameTimer(function() {
-	snake.putFood();
-})
+
+	let game = Game();
+
+	/* Initialize the game */
+	game.passGame(function() {
+		snake.move()
+
+		/* update the game speed whenever the score reaches a specific value */
+		score = parseInt(document.querySelector('.score-val').innerText);
+		if(score>0 && score % 5 == 0 && score !==scoreNow && score != 50)
+		{
+			scoreNow = score;
+			game.upSpeed(5);
+		}
+	})
+
+	/* Start the game */
+	game.startGame()
+	/* Put snake food every 4 seconds */
+	game.gameTimer(function() {
+		snake.putFood();
+	}, 4)
+
+})()
